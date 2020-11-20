58024edf344b8aeed6baef6b2e4ccc1ed8606d69
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2020-11-20 15:28:31 +0100

    produced opposit sign fits

--- scripts/fit_modelS.jl
+++ scripts/fit_modelS.jl
@@ -17,16 +17,16 @@ using TOML
 using DoubleRegge
 
 # bottom-Po model same sign
-settings = Dict(
-    "system" => "compass_ηπ",
-    "pathtodata" => joinpath("data","exp_raw","PLB_shifted"),
-    "fitrange" => [2.4, 3.0],
-    "t2" => -0.2,
-    "tag" => "a2Po-f2Po-PoPo_same-sign",
-    "exchanges" => [1,3,5],
-    "initial_pars" => [0.7, 0.7, 0.0 ],
-    "scale_α" => 0.8,
-)
+# settings = Dict(
+#     "system" => "compass_ηπ",
+#     "pathtodata" => joinpath("data","exp_raw","PLB_shifted"),
+#     "fitrange" => [2.4, 3.0],
+#     "t2" => -0.2,
+#     "tag" => "a2Po-f2Po-PoPo_oppoite-sign",
+#     "exchanges" => [1,3,5],
+#     "initial_pars" => [0.7, 0.7, 0.0 ],
+#     "scale_α" => 0.8,
+# )
 
 # # bottom-Po model opposite sign
 # settings = Dict(
@@ -40,17 +40,30 @@ settings = Dict(
 #     "scale_α" => 0.8,
 # )
 
-# current cesar model
+# cesar model with f2/f2: same sign
 # settings = Dict(
 #     "system" => "compass_ηπ",
 #     "pathtodata" => joinpath("data","exp_raw","PLB_shifted"),
-#     "fitrange" => [2.5, 3.0],
+#     "fitrange" => [2.4, 3.0],
 #     "t2" => -0.2,
-#     "tag" => "a2Po-f2f2-PoPo",
+#     "tag" => "a2Po-f2f2-PoPo_same-sign",
 #     "exchanges" => [1,4,5],
-#     "initial_pars" => [0.7, 1.0, 0.0 ],
+#     "initial_pars" => [0.7, 11.0, 0.0],
 #     "scale_α" => 0.8,
 # )
+
+# cesar model with f2/f2: opposite sign
+settings = Dict(
+    "system" => "compass_ηπ",
+    "pathtodata" => joinpath("data","exp_raw","PLB_shifted"),
+    "fitrange" => [2.4, 3.0],
+    "t2" => -0.2,
+    "tag" => "a2Po-f2f2-PoPo_opposite-sign",
+    "exchanges" => [1,4,5],
+    "initial_pars" => [0.7, -11.0, 0.0],
+    "scale_α" => 0.8,
+)
+
 
 setsystem!(Symbol(settings["system"]))
 
@@ -79,20 +92,21 @@ ellh(pars) = integrate_dcosθdϕ((cosθ,ϕ)->integrand(cosθ,ϕ,pars))[1]
 # 
 integrand′(cosθ,ϕ,pars) = ForwardDiff.gradient(p->integrand(cosθ,ϕ,p), pars)
 ellh′(pars) = integrate_dcosθdϕ((cosθ,ϕ)->integrand′(cosθ,ϕ,pars), dims=3)
-ellh′!(stor,pars) = (stor .= ellh′(pars))
+ellh′!(stor,pars) = (stor .= ellh′(pars)) 
 
 #
 @time ellh([1.0,0,0])
 # [ 0.62, -0.8, +0.0057]
 ft = Optim.optimize(ellh, ellh′!, settings["initial_pars"], BFGS(),
-               Optim.Options(show_trace = true))
+               Optim.Options(show_trace = true, g_tol=1e-4, iterations=15))
 #
 fit_results = Dict(
     "fit_converged" => ft.x_converged,
     "fit_minimizer" => ft.minimizer,
     "fit_minimum" => ft.minimum)
 
-output_name = joinpath("data", "exp_pro","fit-results_$(settings["tag"])_Np=$(length(settings["exchanges"]))_alpha=$(settings["scale_α"]).toml");
+output_name = joinpath("data", "exp_pro", settings["tag"],
+    "fit-results_$(settings["tag"])_Np=$(length(settings["exchanges"]))_alpha=$(settings["scale_α"]).toml");
 
 open(output_name,"w") do io
     TOML.print(io, Dict(
