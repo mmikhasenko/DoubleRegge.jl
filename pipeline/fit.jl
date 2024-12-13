f15c8e9ae3b8f4b4b1efbf81d971e32b45265cb4
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-12-13 17:16:40 +0100

    minor

--- pipeline/fit.jl
+++ pipeline/fit.jl
@@ -1,5 +1,5 @@
 using DrWatson
-@macroexpand(@quickactivate "DoubleRegge")
+@quickactivate "DoubleRegge"
 # 
 using Plots
 # 
@@ -10,10 +10,12 @@ using TOML
 # 
 using DoubleRegge
 
+ARGS
+
 length(ARGS) < 1 && error("provide config file as the first argument!")
 
-settings_file = ARGS[1]
-! isfile(settings_file) && error("no file")
+settings_file = "pipeline/fit_settings.toml"# ARGS[1]
+!isfile(settings_file) && error("no file")
 
 parsed = TOML.parsefile(settings_file)
 settings = parsed["settings"]
@@ -21,34 +23,34 @@ settings = parsed["settings"]
 setsystem!(Symbol(settings["system"]))
 
 # data
-const LMs = compass_ηπ_LMs
+const LMs = getindex.(description_ηπ, 1)
 data = Table(x_IδI_ϕδϕ_compass_ηπ(settings["pathtodata"]))
-amplitudes = [sqrt.(is) .* cis.(ϕs) for (is,ϕs) in zip(data.I, data.ϕ)]
+amplitudes = [sqrt.(is) .* cis.(ϕs) for (is, ϕs) in zip(data.I, data.ϕ)]
 
 # range
-fitrangemap = map(x->inlims(x.x, settings["fitrange"]), data)
+fitrangemap = map(x -> inlims(x.x, settings["fitrange"]), data)
 fitdata = Table(data[fitrangemap], amps = amplitudes[fitrangemap])
 
 # fit
 const exchanges = sixexchages[settings["exchanges"]]
-const model = build_model(exchanges, settings["t2"], settings["scale_α"]; s2shift=settings["s2_shift"])
-intensity(m, cosθ, ϕ; pars) = abs2(model(m, cosθ, ϕ; pars=pars))*q(m)
+const model = build_model(exchanges, settings["t2"], settings["scale_α"]; s2shift = settings["s2_shift"])
+intensity(m, cosθ, ϕ; pars) = abs2(model(m, cosθ, ϕ; pars = pars)) * q(m)
 
-function integrand(cosθ,ϕ,pars)
+function integrand(cosθ, ϕ, pars)
     Id = abs2.(recamp.(cosθ, ϕ, fitdata.amps, Ref(LMs)))
-    Am = model.(fitdata.x, cosθ, ϕ; pars=pars)
+    Am = model.(fitdata.x, cosθ, ϕ; pars = pars)
     Im = abs2.(Am) .* q.(fitdata.x)
     return sum(Im .- Id .* log.(Im))
 end
-ellh(pars) = integrate_dcosθdϕ((cosθ,ϕ)->integrand(cosθ,ϕ,pars))[1]
+ellh(pars) = integrate_dcosθdϕ((cosθ, ϕ) -> integrand(cosθ, ϕ, pars))[1]
 
 # 
-integrand′(cosθ,ϕ,pars) = ForwardDiff.gradient(p->integrand(cosθ,ϕ,p), pars)
-ellh′(pars) = integrate_dcosθdϕ((cosθ,ϕ)->integrand′(cosθ,ϕ,pars), dims=length(pars))
-ellh′!(stor,pars) = (stor .= ellh′(pars)) 
+integrand′(cosθ, ϕ, pars) = ForwardDiff.gradient(p -> integrand(cosθ, ϕ, p), pars)
+ellh′(pars) = integrate_dcosθdϕ((cosθ, ϕ) -> integrand′(cosθ, ϕ, pars), dims = length(pars))
+ellh′!(stor, pars) = (stor .= ellh′(pars))
 
 ft = Optim.optimize(ellh, ellh′!, settings["initial_pars"], BFGS(),
-               Optim.Options(show_trace = true, g_tol=1e-4, iterations=15))
+    Optim.Options(show_trace = true, g_tol = 1e-4, iterations = 15))
 #
 fit_results = Dict(
     "fit_converged" => ft.x_converged,
@@ -57,8 +59,8 @@ fit_results = Dict(
 
 output_name = joinpath("fit-results.toml");
 
-open(output_name,"w") do io
+open(output_name, "w") do io
     TOML.print(io, Dict(
-            "settings"=>settings,
-            "fit_results"=>fit_results))
+        "settings" => settings,
+        "fit_results" => fit_results))
 end
